3771de7f67a50e97a1ca30406e2805f16ac55dbf
Friedrich Götze <fgoetze@multifuchs.email>
2022-02-02 00:37:03 +0100

    Added jlink build support and project for future webui.

---
+++ punsh-soft/core/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module de.fgoetze.punsh.core {
+    requires kotlin.stdlib;
+    exports de.fgoetze.punch.model;
+    exports de.fgoetze.punch.bmp;
+    exports de.fgoetze.punch.svg;
+}
---
+++ punsh-soft/webui/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module de.fgoetze.punsh.webui {
+    requires kotlin.stdlib;
+    requires de.fgoetze.punsh.core;
+    exports de.fgoetze.punsh.webui;
+}
