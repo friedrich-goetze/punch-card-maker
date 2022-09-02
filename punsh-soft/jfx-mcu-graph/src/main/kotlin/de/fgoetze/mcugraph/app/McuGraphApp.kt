b0dc601ca388c33265152ad0d82feb6fb1c3b66c
Dr. Götze <friedrich.goetze@lsc-gmbh.net>
2022-09-02 22:19:56 +0200

    Some progress on graph stuff.

--- punsh-soft/jfx-mcu-graph/src/main/kotlin/de/fgoetze/mcugraph/app/McuGraphApp.kt
+++ punsh-soft/jfx-mcu-graph/src/main/kotlin/de/fgoetze/mcugraph/app/McuGraphApp.kt
@@ -1,16 +1,48 @@
 package de.fgoetze.mcugraph.app
 
 import javafx.application.Application
+import javafx.event.EventHandler
 import javafx.scene.Scene
-import javafx.scene.control.Label
-import javafx.scene.control.TitledPane
+import javafx.scene.chart.LineChart
+import javafx.scene.chart.NumberAxis
+import javafx.scene.chart.XYChart
+import javafx.scene.control.ScrollPane
 import javafx.stage.Stage
+import kotlin.math.sin
 
 class McuGraphApp : Application() {
     override fun start(primaryStage: Stage?) {
         primaryStage!!
         primaryStage.scene = Scene(
-            TitledPane("Hey!", Label("Foo")),
+            ScrollPane().apply {
+                isFitToWidth = true
+                val xAxis = NumberAxis(-10.0, 10.0, 2.0)
+                val yAxis = NumberAxis(-2.0, 2.0, .1)
+                content = LineChart(xAxis, yAxis).apply {
+                    val chartBG = lookup(".chart-plot-background")
+//                    chartBG.setOnMouseMoved { println("${xAxis.getValueForDisplay(it.x)}, ${yAxis.getValueForDisplay(it.y)}") }
+                    chartBG.setOnScroll { e ->
+                        val xRange = xAxis.upperBound - xAxis.lowerBound
+                        val xMid = xAxis.lowerBound + .5 * xRange
+                        val newHalfXRange = .5 * xRange * (1.1 * if (e.deltaY > 0) 1.0 else -1.0)
+//                        xAxis.lowerBound = xMid -
+                        println(e.x)
+                        println(xAxis.getDisplayPosition(e.x))
+                    }
+                    title = "Mein erster Test"
+                    isLegendVisible = true
+                    (xAxis as NumberAxis).lowerBound = -50.0
+                    data += XYChart.Series<Number, Number>().apply {
+                        name = "Cool"
+                        var x = -10.0
+                        while (x <= 10.0) {
+                            data += XYChart.Data(x, sin(x))
+                            x += .1
+                        }
+                    }
+
+                }
+            },
             800.0, 600.0
         )
         primaryStage.title = "Hello"
